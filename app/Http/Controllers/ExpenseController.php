5e399594d58f1366736718bd00c4c2b35b79bfec
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2025-02-14 17:21:17 +0100

    Expenses functions and routes

---
+++ app/Http/Controllers/ExpenseController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Expense;
+use Illuminate\Http\Request;
+
+class ExpenseController extends Controller
+{
+    public function index(Request $request) {
+        $expenses = $request->user()->expenses;
+
+        return response()->json([
+            'data' => $expenses,
+            'success' => true,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'amount' => 'required|numeric',
+            'description' => 'required|string',
+            'category' => 'required|string|in:comida,ocio,electronica,utilidades,ropa,salud,otros',
+        ]);
+
+        $expense = $request->user()->expenses()->create($data, [
+            'user_id' => $request->user()->id,
+        ]);
+
+        return response()->json([
+            'message' => 'Expense created successfully',
+            'data' => $expense,
+            'success' => true,
+        ], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate([
+            'amount' => 'required|numeric',
+            'description' => 'required|string',
+            'category' => 'required|string|in:comida,ocio,electronica,utilidades,ropa,salud,otros',
+        ]);
+
+        $expense = Expense::find($id);
+
+        if ($expense->user_id !== $request->user()->id) {
+            return response()->json([
+                'message' => 'Unauthorized',
+                'success' => false,
+            ], 401);
+        }
+
+        $expense->update($data);
+
+        return response()->json([
+            'message' => 'Expense updated successfully',
+            'data' => $expense,
+            'success' => true,
+        ]);
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $expense = Expense::find($id);
+
+        if ($expense->user_id !== $request->user()->id) {
+            return response()->json([
+                'message' => 'Unauthorized',
+                'success' => false,
+            ], 401);
+        }
+
+        $expense->delete();
+
+        return response()->json([
+            'message' => 'Expense deleted successfully',
+            'success' => true,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ExpenseController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/expenses', [ExpenseController::class, 'index'])->middleware('auth:api');
+Route::post('/expenses', [ExpenseController::class, 'store'])->middleware('auth:api');
+Route::put('/expenses/{id}', [ExpenseController::class, 'update'])->middleware('auth:api');
+Route::delete('/expenses/{id}', [ExpenseController::class, 'destroy'])->middleware('auth:api');
