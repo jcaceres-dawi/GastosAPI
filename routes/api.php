5941026005cf88e2073b074532bac63624ccde87
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2025-02-16 13:09:09 +0100

    Know the user that is in use

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,12 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ExpenseController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/expenses', [ExpenseController::class, 'index'])->middleware('auth:api');
